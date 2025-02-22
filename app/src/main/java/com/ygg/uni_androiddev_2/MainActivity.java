b05ab8b6bf36512a6124e31be2fd54b1a4a9e22c
Ygg <gex_klo@mail.ru>
2025-02-22 09:38:32 +0300

    Clean up some more code in both activities

--- app/src/main/java/com/ygg/uni_androiddev_2/MainActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_2/MainActivity.java
@@ -3,16 +3,11 @@ package com.ygg.uni_androiddev_2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private static final String APP_TAG = "Larioware2";
--- app/src/main/java/com/ygg/uni_androiddev_2/ValidateActivity.java
+++ app/src/main/java/com/ygg/uni_androiddev_2/ValidateActivity.java
@@ -7,11 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ValidateActivity extends AppCompatActivity {
     private final String APP_TAG = "Larioware2";
